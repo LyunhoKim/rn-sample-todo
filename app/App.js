33d27f8a06881c8eed57070ee44d4fcb25a0589d
Lyunho Kim <lyunho.kim@doosan.com>
2020-01-03 16:00:32 +0900

    init

---
+++ app/App.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import { View, ScrollView, StyleSheet } from 'react-native';
+import Heading from './heading';
+import Input from './input';
+import Button from './submitButton';
+import TodoList from './todoList';
+
+let todoIndex = 0;
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputValue: '' ,
+      todos: [],
+      type: 'All'
+    }
+    this.submitTodo = this.submitTodo.bind(this);
+    this.toggleComplete = this.toggleComplete.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
+  }
+
+  componentDidUpdate() {
+    //console.log('State', this.state);
+  }
+
+  inputChange(inputValue) {
+    //console.log('Input Value: ', inputValue);
+    this.setState({ inputValue });
+  }
+
+  submitTodo() {    
+    if(this.state.inputValue.match(/^\s*$/)) {
+      return ;
+    }
+    const todo = {
+      title: this.state.inputValue,
+      todoIndex,
+      complete: false
+    }
+    todoIndex++;
+    const todos = [...this.state.todos, todo];
+    this.setState({
+      todos,
+      inputValue: ''
+    }, () => {
+      
+    })
+  }
+
+  toggleComplete(index) {
+    let todos = this.state.todos.map(
+      todo => { 
+        if(todo.todoIndex === index) 
+          todo.complete = !todo.complete;
+        return todo;      
+      }
+    );    
+    this.setState({ todos });    
+  }
+
+  deleteTodo(index) {    
+    let todos = this.state.todos.filter( todo => todo.todoIndex !== index);    
+    this.setState({ todos });
+  }
+
+  render() {
+    const { inputValue, todos } = this.state;
+    return (
+      <View style={ styles.container }>
+        <ScrollView keyboardShouldPersistTaps='always' style={ styles.content }>
+          <Heading />
+          <Input
+            inputValue={ inputValue }
+            inputChange={ (text) => this.inputChange(text) } />
+            <TodoList todos={ todos } toggleComplete={ this.toggleComplete } deleteTodo={ this.deleteTodo } />
+          <Button submitTodo={ this.submitTodo } />
+        </ScrollView>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5'
+  },
+  content: {
+    flex: 1,
+    paddingTop: 60
+  }
+})
+
+export default App;
---
+++ app/todoList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View } from 'react-native';
+import Todo from './todo';
+
+
+const TodoList = ({ todos, toggleComplete, deleteTodo }) => {
+  todos = todos.map((todo, i) => {
+    return (
+      <Todo 
+        key={ todo.todoIndex }
+        todo={ todo }
+        toggleComplete={ toggleComplete }
+        deleteTodo={ deleteTodo } />
+    )
+  })
+  return (
+    <View>
+      {todos}
+    </View>
+  )
+}
+
+export default TodoList;
